3a12fc7523da05003eeffca70be6f37240e50c7b
Susovan Dev <susovandas.dev@gmail.com>
2025-05-31 09:39:08 +0530

    chore(setup): initialize design service with basic server configuration

---
+++ backend/design-service/src/app.ts
@@ -0,0 +1,33 @@
+import express, { Application } from 'express';
+import { config, morganMiddleware } from '@/config/index';
+import { Logger } from '@/utils';
+import appRoutes from './routes/app.routes';
+
+export class App {
+  app: Application;
+  constructor() {
+    this.app = express();
+  }
+
+  public start() {
+    this.setupMiddlewares();
+    this.setupRoutes();
+    this.severListen();
+  }
+
+  private setupMiddlewares() {
+    this.app.use(morganMiddleware);
+  }
+
+  private setupRoutes() {
+    appRoutes(this.app);
+  }
+
+  private severListen() {
+    this.app.listen(config.port, () => {
+      Logger.info(
+        `Design- service server is running on port: ${config.port} in ${config.env} mode`,
+      );
+    });
+  }
+}
